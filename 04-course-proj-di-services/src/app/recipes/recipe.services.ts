7f5b70aff99d17593279581bbff17530e506507b
Gary Hake <devgaryhake@gmail.com>
2023-04-03 07:36:42 -0400

    DI is done

--- 04-course-proj-di-services/src/app/recipes/recipe.services.ts
+++ 04-course-proj-di-services/src/app/recipes/recipe.services.ts
@@ -1,6 +1,9 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.services';
 import { Recipe } from './recipe.model';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
@@ -8,16 +11,30 @@ export class RecipeService {
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
-      'https://bakesbybrownsugar.com/wp-content/uploads/2022/11/Cherry-Tomato-Confit-6.jpg'
+      'https://bakesbybrownsugar.com/wp-content/uploads/2022/11/Cherry-Tomato-Confit-6.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
     ),
     new Recipe(
       'Another Test Recipe',
       'This is simply a test',
-      'https://bakesbybrownsugar.com/wp-content/uploads/2022/11/Cherry-Tomato-Confit-6.jpg'
+      'https://bakesbybrownsugar.com/wp-content/uploads/2022/11/Cherry-Tomato-Confit-6.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]
     ),
   ];
 
+  constructor(private slService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
