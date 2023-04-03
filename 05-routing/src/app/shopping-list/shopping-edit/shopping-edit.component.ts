b5a92bd07740092e265fd35129054ad2b04b5298
Gary Hake <devgaryhake@gmail.com>
2023-04-03 15:01:17 -0400

    copying project for routes

---
+++ 05-routing/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,22 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.services';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
+})
+export class ShoppingEditComponent {
+  @ViewChild('nameInput', {static: false}) nameInputRef?: ElementRef;
+  @ViewChild('amountInput', {static: false}) amountInputRef?: ElementRef;
+
+  constructor(private slService: ShoppingListService) {}
+
+  onAddItem() {
+    const ingName = this.nameInputRef?.nativeElement.value;
+    const ingAmount = this.amountInputRef?.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.slService.addIngredient(newIngredient);
+  }
+}
