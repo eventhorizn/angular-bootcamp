5d064f426dc7e6181f67eedb94baf9a2244df8fb
Gary Hake <devgaryhake@gmail.com>
2023-05-23 11:42:34 -0400

    done w/ routing

--- 05-routing/src/app/recipes/recipe-list/recipe-list.component.ts
+++ 05-routing/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.services';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,9 +11,17 @@ import { RecipeService } from '../recipe.services';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
 
-  constructor(private reciepeService: RecipeService) {}
+  constructor(
+    private reciepeService: RecipeService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.recipes = this.reciepeService.getRecipes();
+  }
+
+  onNewRecipe() {
+    this.router.navigate(['new'], { relativeTo: this.route });
   }
 }
