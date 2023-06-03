599ec69cea8aff622040c5d89e3786bd932c9c5f
Gary Hake <devgaryhake@gmail.com>
2023-06-03 07:48:48 -0400

    done w/ http stuff

--- 09-course-proj-http/src/app/recipes/recipe.services.ts
+++ 09-course-proj-http/src/app/recipes/recipe.services.ts
@@ -7,23 +7,29 @@ import { Subject } from 'rxjs';
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
+  private recipes: Recipe[] = [];
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://bakesbybrownsugar.com/wp-content/uploads/2022/11/Cherry-Tomato-Confit-6.jpg',
-      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
-    ),
-    new Recipe(
-      'Another Test Recipe',
-      'This is simply a test',
-      'https://bakesbybrownsugar.com/wp-content/uploads/2022/11/Cherry-Tomato-Confit-6.jpg',
-      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'A Test Recipe',
+  //     'This is simply a test',
+  //     'https://bakesbybrownsugar.com/wp-content/uploads/2022/11/Cherry-Tomato-Confit-6.jpg',
+  //     [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+  //   ),
+  //   new Recipe(
+  //     'Another Test Recipe',
+  //     'This is simply a test',
+  //     'https://bakesbybrownsugar.com/wp-content/uploads/2022/11/Cherry-Tomato-Confit-6.jpg',
+  //     [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+  //   ),
+  // ];
 
   constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
