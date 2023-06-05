4db14549ff50e3a96d1d95f895c697047c8d4ba7
Gary Hake <devgaryhake@gmail.com>
2023-06-05 12:11:20 -0400

    error handling

--- 10-course-proj-authentication/src/app/auth/auth.component.ts
+++ 10-course-proj-authentication/src/app/auth/auth.component.ts
@@ -8,6 +8,8 @@ import { AuthService } from './auth.service';
 })
 export class AuthComponent {
   isLoginMode = true;
+  isLoading = false;
+  error: string | null = null;
 
   constructor(private authService: AuthService) {}
 
@@ -21,15 +23,19 @@ export class AuthComponent {
     const email = form.value.email;
     const password = form.value.password;
 
+    this.isLoading = true;
     if (this.isLoginMode) {
       //...
     } else {
       this.authService.signup(email, password).subscribe(
         (res) => {
           console.log(res);
+          this.isLoading = false;
         },
         (err) => {
           console.log(err);
+          this.error = err;
+          this.isLoading = false;
         }
       );
     }
--- 10-course-proj-authentication/src/app/auth/auth.service.ts
+++ 10-course-proj-authentication/src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
 
 interface AuthResponseData {
   kind: string;
@@ -15,13 +16,27 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyANi9qbcYoridQM9VVDphkkkw53tRkKwg8',
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true,
-      }
-    );
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyANi9qbcYoridQM9VVDphkkkw53tRkKwg8',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError((err) => {
+          let errorMessage = 'An unknown error occured';
+          if (!err.error || !err.error.error) return throwError(errorMessage);
+
+          switch (err.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage = 'This email already exists';
+          }
+
+          return throwError(errorMessage);
+        })
+      );
   }
 }
