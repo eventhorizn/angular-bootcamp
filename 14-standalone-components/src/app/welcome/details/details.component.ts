4b430e4bc1d84e1bb76fabf9bd9ef9b99a380044
Gary Hake <devgaryhake@gmail.com>
2023-07-08 22:20:21 -0400

    module complete

--- 14-standalone-components/src/app/welcome/details/details.component.ts
+++ 14-standalone-components/src/app/welcome/details/details.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
+
 import { AnalyticsService } from 'src/app/shared/analytics.service';
 import { HighlightDirective } from 'src/app/shared/highlight.directive';
 
 @Component({
-	standalone: true,
-	imports: [HighlightDirective],
-	selector: 'app-details',
-	templateUrl: './details.component.html',
-	styleUrls: ['./details.component.css'],
+  standalone: true,
+  imports: [HighlightDirective],
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
-	constructor(private analyticsService: AnalyticsService) {}
+  constructor(private analyticsService: AnalyticsService) {}
 
-	onClick() {
-		this.analyticsService.registerClick();
-	}
+  onClick() {
+    this.analyticsService.registerClick();
+  }
 }
